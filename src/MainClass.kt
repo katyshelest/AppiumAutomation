a50c01c37aeb0b5193839462b555dc39022ad1a5
katyshelest <okhlopkova.e@indriver.com>
2022-08-21 01:29:19 +0400

    1 lesson is done

---
+++ src/MainClass.kt
@@ -0,0 +1,18 @@
+open class MainClass {
+
+    private val number = 14
+    private val classNumber = 20
+    private val classString = "Hello, world"
+
+    fun getLocalNumber(): Int {
+        return number
+    }
+
+    fun getClassNumber(): Int {
+        return classNumber
+    }
+
+    fun getClassString(): String {
+        return classString
+    }
+}
