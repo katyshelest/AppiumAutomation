de3faff80f153cb9c91cffbf97c7dd67c50e4b44
Okhlopkova Ekaterina <85631328+katyshelest@users.noreply.github.com>
2022-09-03 12:56:05 +0300

    3lesson ex2 (#3)
    * 1 lesson is done
    
    * 2 lesson
    
    * 2 lesson fix
    
    * prepare
    
    * test for Ex2

--- src/FirstTest.kt
+++ src/FirstTest.kt
@@ -1,10 +1,14 @@
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.android.AndroidDriver
+import junit.framework.Assert.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.net.URL
 
 class FirstTest {
@@ -24,7 +28,7 @@ class FirstTest {
         capabilities.setCapability("app", "/Users/okhlopkovaekaterina/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk")
 
         driver = AndroidDriver(URL("http://127.0.0.1:4723/wd/hub"), capabilities)
-        }
+    }
 
     @After
     fun tearDown() {
@@ -32,7 +36,44 @@ class FirstTest {
     }
 
     @Test
-    fun firstTest() {
-        println("First Test Run")
+    fun checkTextSearch() {
+        waitForElementAndClick(
+            By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+            "Cannot find button 'Skip'",
+            5
+        )
+
+        assertElementHasText(
+            By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+            "Search Wikipedia",
+            "Cannot find main search line",
+            5
+        )
+    }
+
+    private fun waitForElementPresent(by: By, errorMessage: String, timeout: Long): WebElement {
+        val wait = WebDriverWait(driver, timeout)
+        wait.withMessage(errorMessage + "\n")
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by))
+    }
+
+    private fun waitForElementPresent(by: By, errorMessage: String): WebElement {
+        return waitForElementPresent(by, errorMessage, 5)
+    }
+
+    private fun waitForElementAndClick(by: By, errorMessage: String, timeout: Long): WebElement {
+        val element: WebElement = waitForElementPresent(by, errorMessage, timeout)
+        element.click()
+        return element
+    }
+
+    private fun assertElementHasText(by: By, text: String, errorMessage: String, timeout: Long) {
+        val element: WebElement = waitForElementPresent(by, errorMessage, timeout)
+        val textElement = element.text
+        assertEquals(
+            "We see unexpected text",
+            text,
+            textElement
+        )
     }
 }
